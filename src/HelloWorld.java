b2afd287eab915857bc54c9b646125de8e88394f
russelmagdael <r_magdael2025@coloradocollege.edu>
2026-05-04 11:06:11 -0600

    This is russel branch

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 		System.out.println("Hello world!");
 		// This is russel's fingerprint
 		System.out.println("Hello again");
+		System.out.println("This file has went through russel branch");
 	}
-	
 }
